40b0151209d95599164dd4709142a84c892bae27
Kevin Ramirez <61460446+SrHeisen@users.noreply.github.com>
2022-11-11 20:53:33 -0500

    Add files via upload

---
+++ Hello.py
@@ -0,0 +1,9 @@
+# Cometarios en Python "#(comentario)"
+
+# Imprimir en Python
+print("Hola Mundo!")
+
+
+# Crear Variables & Imprimir
+variable = "Hola Mundo!"
+print(variable)
---
+++ if-else.py
@@ -0,0 +1,37 @@
+# If - Else Python
+
+# if condicion
+#   ejecutar codigo si la condicion es True
+# else condicion
+#   Ejecutar codigo si la condicion es False
+
+# Ejercicio Python If - Else
+
+# Dado un número entero, "n", realice las siguientes acciones condicionales:
+# Si "n" es impar, imprimir "Weird"
+# Si "n" es par y en el rango inclusivo de 2 a 5, impresión "Not Weird"
+# Si "n" es par y en el rango inclusivo de 6 a 20, impresión "Weird"
+# Si "n" es par y mayor que 20, impresión "Not Weird"
+
+print("Ingresar numero para identificar el caso de uso de If - ELse")
+n = input()
+
+print(n)
+
+# Simbolos Python
+# < = Menor
+# > = Mayor
+# Operadores Aritmeticos
+
+n = string(entero)
+
+if (n % 2:)
+    print("Weird")
+    if n >= 2 and n <= 5:
+        print("Not Weird")
+        if n >= 6 and n <= 20:
+            print("Weird")
+else:
+    print("Not Weird")
+
+
